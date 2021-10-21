68eae6e097457d303680c623d60dd53c662487be
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-10-21 21:11:41 +0300

    link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { useHistory, useLocation, Route, Link, Switch } from 'react-router-dom';
+import { useHistory, useLocation, Route, Switch } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import YouTubeIcon from '@material-ui/icons/YouTube';
